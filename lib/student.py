40f4b2154f50b765bfae36fe166b03b0c3bc31eb
Tong Tang <pichip22@hotmail.com>
2023-07-16 06:21:00 -0400

    completed

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,8 @@
 from user import User
 
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
+    def learn(self, knowledge):
+        self.knowledge.append(knowledge)
